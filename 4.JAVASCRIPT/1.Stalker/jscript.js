8fb25a19aff08d528357fca37d915d6305d23479
NAVIN GANESH PANDIYAN R K <ganesspandian2@gmail.com>
2020-06-05 13:55:36 +0530

    Add files via upload

---
+++ 4.JAVASCRIPT/1.Stalker/jscript.js
@@ -0,0 +1,9 @@
+var first_name=prompt("Enter your First Name");
+var last_name=prompt("Enter your last Name");
+var age=prompt("Enter your age");
+
+alert("Welcome "+first_name+" "+last_name);
+alert(`Now you\'re ${age}`);
+
+console.log(`Welcome Again! ${first_name+" "+last_name}`);
+console.log(`Now you\'re ${age}`)
